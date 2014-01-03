e795fa0745ab5b7f30f464e5d03fc70daa7dffaf
Sam Wain <slousebox@gmail.com>
2014-01-03 21:28:29 +1100

    Revert "Revert "The performance update""
    This reverts commit 506886f2ab7359d5b174bf52ea33b87e158976fc.

--- src/com/basiqnation/basiqenchant/BasiqEnchantManager.java
+++ src/com/basiqnation/basiqenchant/BasiqEnchantManager.java
@@ -11,6 +11,8 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 public class BasiqEnchantManager {
 
 	private static HashMap<String, Integer> list = new HashMap<String, Integer>();
+	private static Integer count = 0;
+	private static Player player;
 
 	public static void loadArray(HashMap<String, Integer> Enchants) {
 		Set<String> set = Enchants.keySet();
@@ -34,28 +36,47 @@ public class BasiqEnchantManager {
 						error = BasiqEnchant.message.replace("%enchant%",
 								Format.formatEnchant(en));
 					}
-					player.sendMessage(ChatColor.DARK_RED + error + " " + map.get(en));
+					player.sendMessage(ChatColor.DARK_RED + error + " "
+							+ map.get(en));
 				}
 			}
 		}
 	}
+	
+	public static void getPlayer(Player p){
+		player = p;
+	}
+	
+	public static void onClose(Player p){
+		if(p == player){
+			count = 0;
+		}
+	}
 
 	public static void BookDisable(HashMap<String, Integer> map,
-			InventoryClickEvent event, Player player) {
+			InventoryClickEvent event, Player player)
+			throws InterruptedException {
 		Set<String> e = map.keySet();
 		for (String en : e) {
 			if (list.containsKey(en)) {
 				if (!(EnchantLevel.isAllowed(map, list, e))) {
-					event.setCancelled(true);
-
-					String error = "";
-					if (!BasiqEnchant.message.contains("%enchant%")) {
-						error = BasiqEnchant.message;
+					count++;
+					getPlayer(player);
+					if (count > 3) {
+						player.closeInventory();
 					} else {
-						error = BasiqEnchant.message.replace("%enchant%",
-								Format.formatEnchant(en));
+						event.setCancelled(true);
+
+						String error = "";
+						if (!BasiqEnchant.message.contains("%enchant%")) {
+							error = BasiqEnchant.message;
+						} else {
+							error = BasiqEnchant.message.replace("%enchant%",
+									Format.formatEnchant(en));
+						}
+						player.sendMessage(ChatColor.DARK_RED + error + " "
+								+ map.get(en));
 					}
-					player.sendMessage(ChatColor.DARK_RED + error + " " + map.get(en));
 				}
 
 			}
